0e42068492fa70e199b774bd0978a5c5d969ce35
James Bland <98528984+JamesB0010@users.noreply.github.com>
2025-02-16 17:49:52 +0000

    imporved trackpad swipe code fixed bug

--- Artefact/FYP Artefact/Assets/Scripts/eteeRemoteNavigationEvents.cs
+++ Artefact/FYP Artefact/Assets/Scripts/eteeRemoteNavigationEvents.cs
@@ -13,47 +13,48 @@ public class eteeRemoteNavigationEvents
     
     private eteeDevice device;
 
-    private bool ready = true;
+    private bool readyX = true;
+    private bool readyY = true;
     public void Update()
     {
         Vector2 input = this.device.trackpadCoordinates;
 
-        float processedXInput = this.Process1DInput(input.x);
+        float processedXInput = this.Process1DInput(input.x, ref this.readyX);
         
         if (processedXInput == 1)
             this.right?.Invoke();
         else if (processedXInput == -1)
             this.left?.Invoke();
 
-        float processedYInput = this.Process1DInput(input.y);
+        float processedYInput = this.Process1DInput(input.y, ref this.readyY);
         if (processedYInput == 1)
             this.up?.Invoke();
         else if (processedYInput == -1)
             this.down?.Invoke();
     }
-    private float Process1DInput(float input)
+    private float Process1DInput(float input, ref bool readyState)
     {
         float normalizedInput = ((input - 126).MapRange(0, 252, 0, 1) * 2);
         if (normalizedInput >= 0.5f)
         {
-            if (this.ready)
+            if (readyState)
             {
-                this.ready = false;
+                readyState = false;
                 return 1f;
             }
         }
 
         if (normalizedInput <= -0.5f)
         {
-            if (this.ready)
+            if (readyState)
             {
-                this.ready = false;
+                readyState = false;
                 return -1f;
             }
         }
 
         if (normalizedInput == 0)
-            this.ready = true;
+            readyState = true;
 
         return 0;
     }
