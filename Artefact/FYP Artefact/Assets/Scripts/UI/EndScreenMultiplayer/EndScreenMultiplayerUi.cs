58398ea153329125bfa38e10ad9811e7f2a8d3ca
James Bland <98528984+JamesB0010@users.noreply.github.com>
2025-03-06 13:46:42 +0000

    End Screen Multiplayer complete just need to make sure we transition to it wen multiplayer game finises

---
+++ Artefact/FYP Artefact/Assets/Scripts/UI/EndScreenMultiplayer/EndScreenMultiplayerUi.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
+
+public class EndScreenMultiplayerUi : MonoBehaviour
+{
+    private VisualElement root;
+
+    [SerializeField] private PlayerScores playerScores;
+    private SqueezeSelectButton ReturnToMenuButton { get; set; }
+
+    [SerializeField] private UnityEvent ReturnToMenuPressed;
+
+    private void Awake()
+    {
+        this.root = GetComponent<UIDocument>().rootVisualElement;
+
+        this.root.Q<TextElement>("CaughtAmountPlayer1").text = $"You caught {playerScores.Player1Score} fish!";
+
+        this.root.Q<TextElement>("CaughtAmountPlayer2").text = $"You caugt {playerScores.Player2Score} fish!";
+
+        this.ReturnToMenuButton = this.root.Q<SqueezeSelectButton>("ReturnToMainMenu");
+
+        this.ReturnToMenuButton.ButtonPressed += () => { this.ReturnToMenuPressed?.Invoke(); };
+    }
+
+
+    private void Update()
+    {
+        this.ReturnToMenuButton.Player1SqueezeAmount =
+            (uint)FingerTotalForceGetter.GetGenerousPullPercent(eteeAPI.LeftDevice);
+        this.ReturnToMenuButton.Player2SqueezeAmount =
+            (uint)FingerTotalForceGetter.GetGenerousPullPercent(eteeAPI.RightDevice);
+    }
+}
