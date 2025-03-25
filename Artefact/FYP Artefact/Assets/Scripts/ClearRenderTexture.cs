c2f3a9e695d40786cb292da2f27314233ea8906a
James Bland <98528984+JamesB0010@users.noreply.github.com>
2025-03-25 03:02:54 +0000

    got a scene together just need to do crt scanlines

---
+++ Artefact/FYP Artefact/Assets/Scripts/ClearRenderTexture.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ClearRenderTexture : MonoBehaviour
+{
+    [SerializeField] private RenderTexture renderTexture;
+
+    [SerializeField] private Camera camera;
+
+    private void OnEnable()
+    {
+        RenderPipelineManager.endCameraRendering += this.ClearTexture;
+    }
+
+    private void OnDisable(){
+        RenderPipelineManager.endCameraRendering -= this.ClearTexture;
+    }
+
+    private void ClearTexture(ScriptableRenderContext ctx, Camera cam)
+    {
+        if (cam != camera)
+            return;
+        
+        RenderTexture rt = RenderTexture.active;
+        RenderTexture.active = this.renderTexture;
+        GL.Clear(true, true, Color.clear);
+        RenderTexture.active = rt;
+    }
+}
